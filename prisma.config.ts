b7c5f572d4e89477b1e33279daee8b85516f2087
Z User <z@container>
2026-07-10 12:22:33 +0000

    799a1281-ea12-4718-abd1-e3ac585a2944

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -6,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_e5To1bGSkcwR@ep-lucky-dew-atuc6qxu-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_e5To1bGSkcwR@ep-lucky-dew-atuc6qxu-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require",
   },
 });
